c03ce611b453ec20358c4456a620502b8e30b9ad
Hieu Phan <aznrice2k4@gmail.com>
2013-12-31 17:12:01 -0600

    Illusion

--- ill.mk
+++ ill.mk
@@ -1,22 +1,19 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/illusion/config/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+	# Inherit some common ILL stuff.
+$(call inherit-product, vendor/illusion/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d801/d801.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d801
-PRODUCT_NAME := cm_d801
+PRODUCT_NAME := ill_d801
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D801
 PRODUCT_MANUFACTURER := lge
@@ -25,3 +22,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_tmo_us/g2:4.2.2/JDQ39B/
 PRIVATE_BUILD_DESC="g2_tmo_us-user 4.2.2 JDQ39B D80110c.1376399304 release-keys"
 
 PRODUCT_PACKAGES += Torch
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/illusion/bootanimation/Illusion768x768.zip:system/media/bootanimation.zip
