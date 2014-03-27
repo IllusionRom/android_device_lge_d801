f8dc9ca8e576e77a9bda60f62a3ca0585c8f6a46
Snuzzo <jrracinfan@gmail.com>
2014-03-27 16:39:41 -0400

    Proper makefile lunch combos & Boot Ani bring-up

--- ill.mk
+++ ill.mk
@@ -5,8 +5,8 @@ $(call inherit-product, vendor/illusion/config/gsm.mk)
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-	# Inherit some common ILL stuff.
-$(call inherit-product, vendor/illusion/config/common.mk)
+# Inherit some common ILL stuff.
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d801/d801.mk)
@@ -20,8 +20,6 @@ PRODUCT_MANUFACTURER := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_tmo_us/g2:4.2.2/JDQ39B/D80110c.1376399304:user/release-keys
 PRIVATE_BUILD_DESC="g2_tmo_us-user 4.2.2 JDQ39B D80110c.1376399304 release-keys"
-
-PRODUCT_PACKAGES += Torch
 
 # bootanimation
 PRODUCT_COPY_FILES += \
